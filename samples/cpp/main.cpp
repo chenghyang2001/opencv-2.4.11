9f46b4bde7faecac59578aabc3ebd4d270a86cfa
ChengHsien Yang <chenghyang2001@yahoo.com>
2015-12-18 10:42:43 +0800

    add main.cpp init version. can build and run. 10:43  20151218

---
+++ samples/cpp/main.cpp
@@ -0,0 +1,16 @@
+
+#include <opencv2/opencv.hpp>  // full path ~/opencv-2.4.11/include/opencv2/opencv.hpp
+#include <iostream>
+#include <fstream> 
+
+#define aaa cout << "MO: [aaaaaaaaa] FILE=%s " <<  __FILE__  <<  " FUNCTION=%s " <<  __FUNCTION__ <<  " LINE=%d" <<  __LINE__ << endl ;
+
+using namespace cv; 
+using namespace std;
+
+int main( int argc, char** argv )
+{
+
+    cout <<" main" << endl;
+    return 0;
+}
