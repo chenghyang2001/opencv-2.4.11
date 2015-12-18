172861937a7fa928590f1d8fe0865cb70aa35ecb
ChengHsien Yang <chenghyang2001@yahoo.com>
2015-12-18 08:02:28 +0800

    show lena.jpg ok.  08:02  20151218

--- samples/cpp/hello.cpp
+++ samples/cpp/hello.cpp
@@ -7,14 +7,28 @@ using namespace std;
 
 #define aaa cout << "MO: [aaaaaaaaa] FILE=%s " <<  __FILE__  <<  " FUNCTION=%s " <<  __FUNCTION__ <<  " LINE=%d" <<  __LINE__ << endl ;
 
+using namespace cv; 
+using namespace std;
+
 int main( int argc, char** argv )
 {
 
 
     cout <<" hello 3 1 1" << endl;
-    cout <<" hello 3 2 " << endl;
 
+    // Mat img = imread( argv[1], -1 ); 
+    Mat img = imread( "lena.jpg", -1 ); 
 
+    if( img.empty() ) {
 
+	cout << " missing parameter. " << endl;
+	return -1; 
+    }
+
+    namedWindow( "Example2", WINDOW_AUTOSIZE ); 
+    imshow( "Example2", img ); 
+
+    waitKey( 0 ); 
+    destroyWindow( "Example2" ); 
     return 0;
 }
