131e862b1cf3da22be9f526fc2d980d83a511fc3
ChengHsien Yang <chenghyang2001@yahoo.com>
2015-12-19 14:24:03 +0800

    1. study drawing api by reading the this sample file. 14:24  20151219

--- samples/cpp/tutorial_code/core/Matrix/Drawing_1.cpp
+++ samples/cpp/tutorial_code/core/Matrix/Drawing_1.cpp
@@ -1,4 +1,4 @@
-/**
+/** 
  * @file Drawing_1.cpp
  * @brief Simple sample code
  */
@@ -22,28 +22,30 @@ void MyLine( Mat img, Point start, Point end );
  */
 int main( void ){
 
+  // atom image related. >>
   /// Windows names
   char atom_window[] = "Drawing 1: Atom";
-  char rook_window[] = "Drawing 2: Rook";
-
   /// Create black empty images
   Mat atom_image = Mat::zeros( w, w, CV_8UC3 );
-  Mat rook_image = Mat::zeros( w, w, CV_8UC3 );
-
-  /// 1. Draw a simple atom:
-  /// -----------------------
-
   /// 1.a. Creating ellipses
-  MyEllipse( atom_image, 90 );
-  MyEllipse( atom_image, 0 );
-  MyEllipse( atom_image, 45 );
-  MyEllipse( atom_image, -45 );
+//  MyEllipse( atom_image, 90 );
+//  MyEllipse( atom_image, 0 );
+//  MyEllipse( atom_image, 45 );
+//  MyEllipse( atom_image, -45 );
 
   /// 1.b. Creating circles
   MyFilledCircle( atom_image, Point( w/2, w/2) );
 
-  /// 2. Draw a rook
-  /// ------------------
+
+  /// 3. Display your stuff!
+  imshow( atom_window, atom_image );
+  moveWindow( atom_window, 0, 200 );
+  // atom image related. <<
+
+#if 0
+  // rook image >>
+  char rook_window[] = "Drawing 2: Rook";
+  Mat rook_image = Mat::zeros( w, w, CV_8UC3 );
 
   /// 2.a. Create a convex polygon
   MyPolygon( rook_image );
@@ -55,18 +57,16 @@ int main( void ){
          Scalar( 0, 255, 255 ),
          -1,
          8 );
-
-  /// 2.c. Create a few lines
+  
   MyLine( rook_image, Point( 0, 15*w/16 ), Point( w, 15*w/16 ) );
   MyLine( rook_image, Point( w/4, 7*w/8 ), Point( w/4, w ) );
   MyLine( rook_image, Point( w/2, 7*w/8 ), Point( w/2, w ) );
   MyLine( rook_image, Point( 3*w/4, 7*w/8 ), Point( 3*w/4, w ) );
 
-  /// 3. Display your stuff!
-  imshow( atom_window, atom_image );
-  moveWindow( atom_window, 0, 200 );
   imshow( rook_window, rook_image );
   moveWindow( rook_window, w, 200 );
+  // rook image <<
+#endif 
 
   waitKey( 0 );
   return(0);
