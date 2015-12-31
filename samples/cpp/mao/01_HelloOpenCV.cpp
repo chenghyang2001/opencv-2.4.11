994f5e15cc1f89d9eb38839de625b05df2f928cd
ChengHsien Yang <chenghyang2001@yahoo.com>
2015-12-31 09:31:25 +0800

    code clean at 09:31  20151231

--- samples/cpp/mao/01_HelloOpenCV.cpp
+++ samples/cpp/mao/01_HelloOpenCV.cpp
@@ -7,10 +7,12 @@ using namespace std;
 int main()
 {
 	//【3】記錄起始時間
+//        double time0 = static_cast<double>(getTickCount());  
 	double time0 = static_cast<double>(getTickCount());  
 
 	// 【1】讀入一張圖形 
-	Mat img=imread("/home/peter/opencv-2.4.11/samples/cpp/mao/1.jpg");
+//        Mat img=imread("/home/peter/opencv-2.4.11/samples/cpp/mao/1.jpg");
+	Mat img=imread("/home/peter/opencv-2.4.11/samples/cpp/mao/2.jpg");
 
 	// 【2】在視窗中顯示載入的圖形
 	imshow("【載入的圖形】",img);
@@ -19,7 +21,7 @@ int main()
 	// 【3】等待6000 ms後視窗自動關閉
 	cout << " rows =  " << img.rows   << endl ; 
 	cout << " cols =  " << img.cols   << endl ; 
-//        cout << " type =  " << img.type() << endl ; 
+	cout << " type =  " << img.type() << endl ; 
 	//【5】計算執行時間並輸出
 	time0 = ((double)getTickCount() - time0)/getTickFrequency();
 	cout<<"\t此方法執行時間為： "<<time0<<"秒"<<endl;  //輸出執行時間
--- samples/cpp/mao/02_ShowImage.cpp
+++ samples/cpp/mao/02_ShowImage.cpp
@@ -8,8 +8,8 @@ int main( )
 {    
 	// 【1】讀入一張圖形，載入圖像
 //        Mat srcImage = imread("/home/peter/opencv-2.4.11/samples/cpp/mao/1.jpg");
-//        Mat srcImage = imread("/home/peter/opencv-2.4.11/samples/cpp/mao/1.jpg",CV_LOAD_IMAGE_GRAYSCALE); //  CV_LOAD_IMAGE_GRAYSCALE
-	Mat srcImage = imread("/home/peter/opencv-2.4.11/samples/cpp/mao/1.jpg",CV_LOAD_IMAGE_COLOR ); //  CV_LOAD_IMAGE_GRAYSCALE
+	Mat srcImage = imread("/home/peter/opencv-2.4.11/samples/cpp/mao/1.jpg",CV_LOAD_IMAGE_GRAYSCALE); //  CV_LOAD_IMAGE_GRAYSCALE
+//        Mat srcImage = imread("/home/peter/opencv-2.4.11/samples/cpp/mao/1.jpg",CV_LOAD_IMAGE_COLOR ); //  CV_LOAD_IMAGE_GRAYSCALE
 
 	// 【2】顯示載入的圖形
 	imshow("【原始圖】",srcImage);
