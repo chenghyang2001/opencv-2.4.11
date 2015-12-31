eb12da81531783b6550ace534ddb4d6c1e7bef16
ChengHsien Yang <chenghyang2001@yahoo.com>
2015-12-31 09:45:09 +0800

    add my own peter.c to experiment c file. 09:45  20151231

---
+++ samples/c/peter.c
@@ -0,0 +1,11 @@
+
+#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/highgui/highgui_c.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("1. peter \n");
+    printf("2. peter \n");
+    return 0;
+}
